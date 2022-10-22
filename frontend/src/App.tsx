387505c691840847dbeb19ee009c603903699e77
Andrielson Silva <andrielson@gmail.com>
2022-10-22 16:47:19 -0300

    [FE] Add user list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,31 @@
+import {gql, useQuery} from "@apollo/client";
+
+type User = {
+  id: string;
+  name: string;
+}
+
+const GET_USER = gql`
+    query {
+      users {
+        id
+        name
+      }
+    }
+`
+
 function App() {
+  const {data, loading} = useQuery<{users: User[]}>(GET_USER);
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <h1>Hello World</h1>
+    <ul>
+      {data?.users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+
   )
 }
 
