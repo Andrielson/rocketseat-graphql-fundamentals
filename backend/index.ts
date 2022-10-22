c83e795ebaffa50f961c01d98832045a6d134276
Andrielson Silva <andrielson@gmail.com>
2022-10-22 18:23:07 -0300

    [BE] Fix schema filename

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,7 @@ import {UserResolver} from "./src/resolvers/UserResolver";
 async function main() {
     const schema = await buildSchema({
         resolvers: [UserResolver],
-        emitSchemaFile: path.resolve(__dirname, 'schema.sql')
+        emitSchemaFile: path.resolve(__dirname, 'schema.gql')
     });
 
     const server = new ApolloServer({schema});
